3be975de4df4998b0de675ecf76186383cd1f5bd
Lucian Haj <lucianhaj264@gmail.com>
2021-03-03 15:45:30 -0800

    added second test for testing Fizz for multiples of 3 only

--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -9,6 +9,9 @@ class TestFizzBuzz(unittest.TestCase):
         self.assertEqual(FizzBuzz.fizznbuzz(4), 4)
         self.assertEqual(FizzBuzz.fizznbuzz(100), 100)
 
+    def test_fizz(self):
+        self.assertEqual(FizzBuzz.fizznbuzz(3), "Fizz")
+        self.assertEqual(FizzBuzz.fizznbuzz(6), "Fizz")
 
 if __name__ == "__main__":
     unittest.main()
