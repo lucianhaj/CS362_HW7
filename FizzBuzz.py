a9e99297659868bf49fd123c67c6bc13bf23ab35
Lucian Haj <lucianhaj264@gmail.com>
2021-03-03 16:05:44 -0800

    Fizz, Buzz, and regular outputs all work wit an if, elif, and else conditional flow

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -4,11 +4,6 @@ def fizznbuzz(n):
     elif n % 5 == 0:
         return("Buzz")
     else:
-        if n == 1:
-            return 1
-        elif n == 4:
-            return 4
-        else:
-            return 100
+        return n
 
 
