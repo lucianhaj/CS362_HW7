eb045d3756a2a8200e6cb831e25a8927446ddaa3
Lucian Haj <lucianhaj264@gmail.com>
2021-03-03 16:14:40 -0800

    Changed the output location so that not only the tests, but also program requirements were satisfied

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,4 +1,15 @@
 def fizznbuzz(n):
+
+    for i in range(1, 100):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fuzz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
     if n % 3 == 0 and n % 5 == 0:
         return ("FizzBuzz")
     elif n % 3 == 0:
@@ -9,3 +20,4 @@ def fizznbuzz(n):
         return n
 
 
+fizznbuzz(3)
