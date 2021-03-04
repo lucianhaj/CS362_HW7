850b3b009942478907b62391fc8621d0698ae0d5
Lucian Haj <lucianhaj264@gmail.com>
2021-03-03 18:24:50 -0800

    Added to conditional structure which makes sure that if the year is divisible by 400 and 100 it is considered a leap year

--- LeapYear.py
+++ LeapYear.py
@@ -1,9 +1,13 @@
 def leap(n):
     if n < 4:
         return "Not a Leap Year!"
-    if n % 100 == 0:
-        return "Not a Leap Year!"
     elif n % 4 == 0:
-        return "Leap Year!"
+        if n % 100 == 0:
+            if n % 400 == 0:
+                return "Leap Year!"
+            else:
+                return "Not a Leap Year!"
+        else:
+            return "Leap Year!"
     else:
         return "Not a Leap Year!"
