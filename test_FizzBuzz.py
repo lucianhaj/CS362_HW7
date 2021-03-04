ac963caa9218866513517cc24f4bd364cd5a5353
Lucian Haj <lucianhaj264@gmail.com>
2021-03-03 16:04:09 -0800

    Changed test case because of incorrect calculation.

--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -7,8 +7,6 @@ class TestFizzBuzz(unittest.TestCase):
     def test_normal(self):
         self.assertEqual(FizzBuzz.fizznbuzz(1), 1)
         self.assertEqual(FizzBuzz.fizznbuzz(4), 4)
-        self.assertEqual(FizzBuzz.fizznbuzz(100), 100)
-
     def test_fizz(self):
         self.assertEqual(FizzBuzz.fizznbuzz(3), "Fizz")
         self.assertEqual(FizzBuzz.fizznbuzz(6), "Fizz")
